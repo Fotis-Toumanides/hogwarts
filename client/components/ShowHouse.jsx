03f566bb21244f923a903e99b76f3ba4aeaaedc5
Fotis-Toumanides <fotis.toumanides@gmail.com>
2024-03-04 01:00:29 -0800

    first commit

---
+++ client/components/ShowHouse.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+
+export default function ShowHouse({ house }) {
+
+    function isColorNameSupported(colorName) {
+        let checkColor = colorName.toLowerCase()
+
+        const allColors = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkgrey", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "grey", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"];
+        if (allColors.includes(checkColor)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    // Checking if houseColors are recognized by browsers.
+    let houseColours = house.houseColours.split(" and ")
+    let colorOne, colorTwo
+
+    if (isColorNameSupported(houseColours[0]) && isColorNameSupported(houseColours[1])) {
+        colorOne = houseColours[0].toLowerCase()
+        colorTwo = houseColours[1].toLowerCase()
+    } else {
+        colorOne = "white"
+        colorTwo = "black"
+    }
+    return (
+        <dialog open>
+            <div className='outter-div'>
+                <div className='w-2/3 sm:w-full rounded shadow-lg m-4'>
+                    <div>
+                        <div className='d-flex justify-content-between p-4'>
+                            <h3 className='house-name'>{house.name}</h3>
+                            <h5>{house.animal}</h5>
+                        </div>
+                        <div className="colorDiv" style={{ background: `linear-gradient(90deg, ${colorOne} 0%, ${colorTwo} 100%)` }} ></div>
+                    </div>
+                    <h5 className='p-4'>Founder: <strong>{house.founder}</strong></h5>
+                </div>
+            </div>
+            <form method="dialog" action="">
+                <button>Close</button>
+            </form>
+        </dialog>
+    )
+
+
+}
---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import HousesList from "../components/HousesList";
+import ShowHouse from "../components/ShowHouse";
+import React, { useEffect, useState } from 'react'
+import './index.css'
+
+
+function App() {
+  const [houses, setHouses] = useState([])
+  const [searchHouse, setSearchHouse] = useState('')
+  const [showContent, setShowContent] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:3300/houses')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setHouses(data)
+
+      })
+  }, [])
+
+  function handleSearch(e) {
+    let lowName
+    if (e.keyCode === 13) {
+      houses.map((house) => {
+        lowName = house.name
+        if (lowName.toLowerCase().match(searchHouse)) {
+          setShowContent(house)
+        }
+      })
+    }
+  }
+  return (
+    <>
+      {showContent && <ShowHouse house={showContent} />}
+      <section className="h-screen my-8 flex flex-row gap-8">
+        <div className="flex items-center mt-4 p-4 gap-2">
+          <label className="text-md m-3">Search for a house</label>
+          <input type="text" onChange={e => setSearchHouse(e.target.value)} value={searchHouse} onKeyDown={handleSearch} />
+        </div>
+        <HousesList houses={houses} />
+      </section>
+    </>
+  );
+}
+
+export default App;
